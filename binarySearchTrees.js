b87a957ba3a7b16689818617ed0935ae80d08dc1
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-09-13 16:46:10 +0700

    Add prettyPrint function to class Tree

--- binarySearchTrees.js
+++ binarySearchTrees.js
@@ -63,5 +63,17 @@ class Tree {
         return node;
     }
 
+    prettyPrint(node, prefix = '', isLeft = true) {
+        if (node.right !== null) {
+            prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+        }
+        
+        console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+        
+        if (node.left !== null) {
+            prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+        }
+    }
 
+    
 }
