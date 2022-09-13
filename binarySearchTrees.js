5cb4f881b6a13731bac4d54288facf54397345fa
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-09-13 16:35:31 +0700

    add mergSort function

--- binarySearchTrees.js
+++ binarySearchTrees.js
@@ -6,9 +6,48 @@ class Node {
     }
 }
 
+function mergeSort(arr) {
+    if (arr.length < 2) {
+        return arr;
+    } else {
+        let sorted = [];
+
+        let left = mergeSort(arr.slice(0, arr.length / 2));
+        let right = mergeSort(arr.slice(arr.length / 2));
+
+        console.log(left);
+        console.log(right);
+
+        let leftPointer = 0;
+        let rightPointer = 0;
+        
+        while (sorted.length < left.length + right.length) {
+            if (rightPointer >= right.length) {
+                sorted.push(left[leftPointer]);
+                leftPointer++;
+            } else if (leftPointer >= left.length) {
+                sorted.push(right[rightPointer]);
+                rightPointer++;
+            } else if (left[leftPointer] < right[rightPointer]) {
+                sorted.push(left[leftPointer]);
+                leftPointer++;
+            } else {
+                sorted.push(right[rightPointer]);
+                rightPointer++;
+            }
+        }
+    
+        return sorted;
+    }
+}
+
 class Tree {
-    constructor(sortedArr) {
-        this.sortedArr = sortedArr;
+    constructor(arr) {
+        this.arr = arr;
         this.root = null;
+    }
+
+    buildTree(arr) {
+
     }
 }
